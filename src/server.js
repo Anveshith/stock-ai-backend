b0356201bbf297b5ba5f4e691ac81b6b2ed66d56
Anveshith <anveshithsadala@gmail.com>
2026-01-15 19:36:30 +0530

    fix cron path for Render src/src layout

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();   // ✅ MUST BE FIRST
 
 const cron = require("node-cron");
-const { runDailyJob } = require("../srp/cron/dailyJob");
+const { runDailyJob } = require("../src/cron/dailyJob");
 
 const express = require("express");
 const cors = require("cors");
